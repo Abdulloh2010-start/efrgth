a61856bcf12aea6acf9dd41d2bcc877203492c0d
Abdulloh2010-start <abdullohlalala@gmail.com>
2024-12-12 21:41:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = prompt('String typedagi malumot kiriting');
+let b = Number(prompt('Number typedagi malumot kiriting'));
+let c = prompt('Boolean typedagi malumot kiriting') === 'true';
+
+alert('Hamma malumotlarni togri kiritganizga ishonchingiz komilmi?');
+
+let a1 = typeof a;
+let b1 = typeof b;
+let c1 = typeof c;
+
+alert(`Siz kiritgan "${a}" sozi ${a1} typega tegishli
+Siz kiritgan "${b}" sozi ${b1} typega tegishli
+Siz kiritgan "${c}" sozi ${c1} typega tegishli`);
